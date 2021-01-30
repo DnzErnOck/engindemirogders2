ee7c411a80e9a7657febbf183ebe19cd7c51f0dd
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2021-01-30 12:10:13 +0300

    .

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -14,7 +14,14 @@ namespace GenericsIntro
         }
         public void Add(T item)
         {
+            T[] TempArray = items;   
+            items = new T[items.Length + 1];
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                items[i] = TempArray[i];
+            }
 
+            items[items.Length - 1] = item;
         }
     }
 }
