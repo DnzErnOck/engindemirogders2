7da52539a260e46bf523303af8ba4ec294f2031b
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2021-01-30 16:37:46 +0300

    .

---
+++ MyDictionaryExemple/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryExemple
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keys;
+        T2[] values;
+
+        
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+        public void Add(T1 key,T2 value)
+        {
+            T1[] TempArray = keys;
+            T2[] TempArray2 = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                keys[i] = TempArray[i];
+                values[i] = TempArray2[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public T1[] key
+        {
+            get { return keys; }
+        }
+        public T2[] value
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionaryExemple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryExemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenci = new Dictionary<int, string>();
+            
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Deniz");
+            foreach (var item in myDictionary.value)
+            {
+                Console.WriteLine(item);
+            }
+
+            //foreach (KeyValuePair<int, string> kvp in myDictionary)
+            //{
+            //    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            //}
+        }
+    }
+}
